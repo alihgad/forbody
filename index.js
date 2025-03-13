09b96c08f2fd6a21ec5848164d916ba91d29aff7
Ali <alihgad2@gmail.com>
2025-03-13 14:12:30 +0200

    home

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ app.use(express.json())
 app.use("/user", userRouter)
 app.use("/product", productRouter)
 app.use("/cart", cartRouter)
+app.get('/', (req, res) => res.json({
+    msg:" Hello this is ForBody API"
+}))
 
 app.use("*", (req, res , next) =>next(new AppError("route not found",404)))
 app.use((err, req, res, next) => {
     console.log(err.message)
     return res.status(err.statusCode || err["statusCode"] || 500).json({ message: err.message, stack: err.stack })
 })
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
